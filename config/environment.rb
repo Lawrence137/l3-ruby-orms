aeefc739b7e50a87d0877a6088fceb3e42a22f46
Lawrence137 <lawrencekimani18@gmail.com>
2023-02-21 11:36:29 +0300

    complete

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,36 @@
 require 'bundler'
 Bundler.require
 
+DB = { conn: SQLite3::Database.new("db/school.db") }
 require_relative '../lib/student'
 
-DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+
+
+#reset table
+Student.reset
+
+# create table
+Student.create_table
+
+
+
+# create student data
+s1 = Student.new(name: 'Jane Doe', age: 28)
+s2 = Student.new(name: 'John Doe', age: 40)
+s3 = Student.new(name: 'Simba Marara', age: 50)
+
+# save student data
+s1.create
+s2.create
+s3.create
+
+pp " Student 1: #{s1}"
+
+
+# view student data
+pp Student.all
+
+print 'SEARCH RESULTS: '
+pp Student.search_by(name: 'Simba Marara')
--- lib/student.rb
+++ lib/student.rb
@@ -4,21 +4,115 @@ class Student
     def initialize(name:, age:, id: nil)
         @id=id
         @name=name
-        @id=id
+        @age=age
     end
 
 # TODO: CREATE TABLE
+    def self.create_table
+
+        query = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL, age INTEGER NOT NULL)"
+
+        DB[:conn].query(query)
+
+    end
 
 # TODO: INSERT RECORD
+    def create
+        query = <<-SQL
+            INSERT INTO students (name, age) VALUES (?, ?)
+        SQL
+
+        DB[:conn].query(query, self.name, self.age)
+
+        setup_id
+    end 
+
 
 # TODO: SHOW ALL RECORDS
+    def self.all
+
+        query = <<-SQL
+            SELECT * FROM students
+        SQL
+
+        DB[:conn].query(query).map do |row|
+            convert_to_object(row)
+        end
+
+    end
 
 # TODO: UPDATE RECORD
+    def update
+
+        query = <<-SQL
+            UPDATE students SET name = ?, age = ? WHERE name = ?
+        SQL
+
+        DB[:conn].query(query, self.name, self.age, self.name)
+
+    end
+
 
 # TODO: DELETE RECORD
+    def destroy
+        query = <<-SQL 
+            DELETE FROM students WHERE name = ?
+        SQL
+
+        DB[:conn].query(query, self.name)
+    end
+
 
 # TODO: CONVERT TABLE RECORD TO RUBY OBJECT
 
+def self.convert_to_object(row)
+    self.new(name: row[1], age: row[2], id: row[0])
+end
+
+def self.reset
+    query = <<-SQL
+       DROP TABLE students
+    SQL
+    DB[:conn].query(query)
+end
+
 # TODO: SEARCH FOR RECORD THAT MEETS CERTAIN CONDITIONS
+def self.search_by(name: nil, age: nil)
+    data = if name && age
+        q = "SELECT * FROM students WHERE name = ? AND age = ?"
+        DB[:conn].query(q, name, age) 
+    elsif name
+        q = 'SELECT * FROM students WHERE name = ?'
+        # q = "SELECT * FROM students WHERE name LIKE %#{name}%"
+        DB[:conn].query(q, name) 
+    elsif age 
+       q = 'SELECT * FROM students WHERE age = ?'
+       DB[:conn].query(q, age) 
+    else
+        q = 'SELECT * FROM students'
+        DB[:conn].query(q) 
+    end
+
+    data.map do |row|
+        convert_to_object(row)
+    end
+
+    # DB[:conn].query(guery) 
+
+
+end
+
+
+private 
+
+def setup_id
+    query = <<-SQL
+
+       SELECT last_insert_rowid() FROM students
+    SQL
+    self.id = DB[:conn].query(query)
+end
+
+
 
 end
